07bbf52b62d1794c3a9cfd8e2ffc33cebb6564f7
Thomas Kim <Thomasjk0831@gmail.com>
2020-09-18 11:14:02 -0400

    installed dependcies. setup form

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
+import { useHistory } from 'react-router-dom'
 
-const Login = () => {
+const userLogin = {
+  username : '',
+  password : ''
+}
+
+const Login = (props) => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+  const [input, setInput] = useState(userLogin)
+
+  const changeHandler = (e) => {
+    setInput({
+      ...input,
+      [e.target.name] : e.target.value
+    })
+  }
+
+  const submitHandler = (e) => {
+    e.preventDefault()
+  }
+
   return (
     <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
+       <div>
+            <form onSubmit={submitHandler}>
+                <h2>Log-in</h2>
+                <label> <div>username</div>
+                    <input 
+                      name= 'username'
+                      type = 'text'
+                      value = {input.username}
+                      onChange = {changeHandler}
+                      />
+                </label>
+                <label> <div>password</div>
+                    <input 
+                      name= 'password'
+                      type = 'text'
+                      value = {input.password}
+                      onChange = {changeHandler}
+                      />
+                </label>
+                <button>Submit</button>
+            </form>
+        </div>
     </>
   );
 };
